90bd9684488067fad39db4f6f216f5e6b65588bd
AnandJeyakumar <janand819@gmail.com>
2025-12-08 00:54:24 +0400

    Fixed YML FIle

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,72 +1,74 @@
 import { defineConfig, devices } from '@playwright/test';
 
-
 export default defineConfig({
   testDir: './tests',
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
-  // reporter: 'html',
-  reporter: [['list'], ['allure-playwright', { outputFolder: 'allure-results' , disableHistory: true}]],
+
+  reporter: [
+    ['list'],
+    ['allure-playwright', {
+      outputFolder: 'allure-results',
+      disableHistory: true
+    }]
+  ],
+
   use: {
-    baseURL : 'https://trade.multibank.io/',
-    headless: true, 
+    baseURL: 'https://trade.multibank.io/',
+    headless: true,
     trace: 'on-first-retry',
-    video: 'retain-on-failure', 
+    video: 'retain-on-failure',
     screenshot: 'only-on-failure'
   },
 
-  /* Configure projects for major browsers */
   projects: [
+    // ---------- CHROMIUM ----------
     {
       name: 'chromium',
-      use: { ...devices['Desktop Chrome'] ,
-      viewport: { width: 1520, height: 1080 },
-      launchOptions: {
-        args: [
-          '--window-size=1520,1080',
-          '--disable-dev-shm-usage',
-          '--no-sandbox',
-          '--disable-setuid-sandbox',
-          '--disable-gpu'
-        ]
+      use: {
+        ...devices['Desktop Chrome'],
+        viewport: { width: 1520, height: 1080 },
+        launchOptions: {
+          args: [
+            '--window-size=1520,1080',
+            '--disable-dev-shm-usage',
+            '--no-sandbox',
+            '--disable-setuid-sandbox',
+            '--disable-gpu'
+          ]
+        }
       }
-
- },
-    
     },
 
-    // {
-    //   name: 'firefox',
-    //   use: { ...devices['Desktop Firefox'] },
-    // },
+    // ---------- FIREFOX ----------
+    {
+      name: 'firefox',
+      use: {
+        ...devices['Desktop Firefox'],
+        viewport: { width: 1520, height: 1080 },
+        launchOptions: {
+          args: [
+            '-width=1520',
+            '-height=1080'
+          ]
+        }
+      }
+    },
 
-    // {
-    //   name: 'webkit',
-    //   use: { ...devices['Desktop Safari'] },
-    // },
-
-    /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
-
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    // },
-  ],
-
-  
+    // ---------- WEBKIT ----------
+    {
+      name: 'webkit',
+      use: {
+        ...devices['Desktop Safari'],
+        viewport: { width: 1520, height: 1080 },
+        launchOptions: {
+          args: [
+            '--window-size=1520,1080'
+          ]
+        }
+      }
+    },
+  ]
 });
