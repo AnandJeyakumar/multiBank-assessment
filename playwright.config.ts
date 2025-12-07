74d2b9419ae8ca3cd92c388f3ae2cecf051b29ee
AnandJeyakumar <janand819@gmail.com>
2025-12-08 00:32:00 +0400

    Removed .only from test file

--- playwright.config.ts
+++ playwright.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
   // reporter: 'html',
-  reporter: [['list'], ['allure-playwright', { outputFolder: 'allure-results' }]],
+  reporter: [['list'], ['allure-playwright', { outputFolder: 'allure-results' , disableHistory: true}]],
   use: {
     baseURL : 'https://trade.multibank.io/',
     headless: true, 
